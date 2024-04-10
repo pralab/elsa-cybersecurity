64ab05eb6cb78e57ebc1a3bbe9c9ec0e8123ab13
Angelo Sotgiu <angelo.sotgiu@unica.it>
2024-04-10 14:55:07 +0200

    update secsvm script

--- track_3/secsvm_track_3.py
+++ track_3/secsvm_track_3.py
@@ -1,7 +1,8 @@
 import json
 import os
-from src.models import SecSVM
-from src.models.utils import *
+import models
+from models import SecSVM
+from models.utils import *
 
 
 if __name__ == "__main__":
@@ -15,36 +16,38 @@ if __name__ == "__main__":
     """
     classifier = SecSVM(C=0.1, lb=-0.5, ub=0.5)
 
-    base_path = os.path.dirname(__file__)
+    model_base_path = os.path.join(os.path.dirname(models.__file__), "../..")
+    base_path = os.path.join(os.path.dirname(__file__))
 
     clf_path = os.path.join(
-        base_path, "../pretrained/secsvm_classifier.pkl")
+        model_base_path, "pretrained/secsvm_classifier.pkl")
     vect_path = os.path.join(
-        base_path, "../pretrained/secsvm_vectorizer.pkl")
+        model_base_path, "pretrained/secsvm_vectorizer.pkl")
 
     if os.path.exists(clf_path) and os.path.exists(vect_path):
         classifier = SecSVM.load(vect_path, clf_path)
     else:
         features_tr = load_features(
-            os.path.join(base_path, "data/training_set_features.zip"))
+            os.path.join(base_path, "../data/training_set_features.zip"))
         y_tr = load_labels(
-            os.path.join(base_path, "data/training_set_features.zip"),
-            os.path.join(base_path, "data/training_set.zip"))
+            os.path.join(base_path, "../data/training_set_features.zip"),
+            os.path.join(base_path, "../data/training_set.zip"))
         classifier.fit(features_tr, y_tr)
         classifier.save(vect_path, clf_path)
 
     results = []
     for i in range(1, 5):
         features_ts = load_features(
-            os.path.join(base_path, f"data/test_set_features_round_{i}.zip"))
+            os.path.join(
+                base_path, f"../data/test_set_features_round_{i}.zip"))
         y_pred, scores = classifier.predict(features_ts)
         results.append({
             sha256: [int(y), float(s)] for sha256, y, s in zip(
                 load_sha256_list(os.path.join(
-                    base_path, f"data/test_set_features_round_{i}.zip")),
+                    base_path, f"../data/test_set_features_round_{i}.zip")),
                 y_pred, scores)})
 
-    with open(os.path.join(base_path,
-                           "submissions/submission_secsvm_track_3.json"),
-              "w") as f:
+    with open(os.path.join(
+            base_path, "submissions/submission_secsvm_track_3.json"),
+            "w") as f:
         json.dump(results, f)
